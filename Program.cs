65e8133f25f70eb91eb48e1fca2569663653e69b
Billy Blackman <blackmanbilly@gmail.com>
2020-05-28 15:52:14 -0500

    Completed exercise 2

--- Program.cs
+++ Program.cs
@@ -9,14 +9,41 @@ namespace linq
         static void Main(string[] args)
         {
 
-            // Find the words in the collection that start with the letter 'L'
-            List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
+            // // Find the words in the collection that start with the letter 'L'
+            // List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
 
-            IEnumerable<string> LFruits = fruits.Where(fruits => fruits.StartsWith("L"));
+            // IEnumerable<string> LFruits = fruits.Where(fruits => fruits.StartsWith("L"));
 
-            List<string> LFruitsList = LFruits.ToList();
+            // List<string> LFruitsList = LFruits.ToList();
 
-            LFruitsList.ForEach(fruit => Console.WriteLine(fruit));
+            // LFruitsList.ForEach(fruit => Console.WriteLine(fruit));
+
+            // Which of the following numbers are multiples of 4 or 6
+
+            List<int> numbers = new List<int>()
+            {
+                15,
+                8,
+                21,
+                24,
+                32,
+                13,
+                30,
+                12,
+                7,
+                54,
+                48,
+                4,
+                49,
+                96
+            };
+
+            IEnumerable<int> fourSixMultiples = numbers.Where(number => number % 6 == 0 || number % 4 == 0);
+
+            List<int> fourSixMultiplesList = fourSixMultiples.ToList();
+
+            fourSixMultiplesList.ForEach(number => Console.WriteLine(number));
+
         }
     }
 }
