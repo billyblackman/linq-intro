d864d7d8f068b9c84323f0b33393f0c7231092d8
Billy Blackman <blackmanbilly@gmail.com>
2020-05-28 15:41:02 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Find the words in the collection that start with the letter 'L'
+            List<string> fruits = new List<string>() { "Lemon", "Apple", "Orange", "Lime", "Watermelon", "Loganberry" };
+
+            IEnumerable<string> LFruits = fruits.Where(fruits => fruits.StartsWith("L"));
+
+            List<string> LFruitsList = LFruits.ToList();
+
+            LFruitsList.ForEach(fruit => Console.WriteLine(fruit));
+        }
+    }
+}
